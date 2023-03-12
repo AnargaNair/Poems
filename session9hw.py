e49350dbd28bd2984c99925246597a3eabf62c96
AnargaNair <105276878+AnargaNair@users.noreply.github.com>
2023-03-12 10:36:06 +0100

    Add files via upload

---
+++ session9hw.py
@@ -0,0 +1,7 @@
+file = open("session9poem.txt","w")
+file.write(" \nTwo roads diverged in a yellow wood,\n")
+file.write("And sorry I could not travel both And be one traveler,\n")
+file.write("long I stood And looked down one as far as I could\n")
+file.write("To where it bent in the undergrowth;\n")
+file.write("Anarga Nair ")
+file.close()
